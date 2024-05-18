757700a32ee7a1b5cae265cb3ab46624307fc3ed
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 02:15:13 +0700

    add solution

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,15 @@
 def fibonacci(number):
-    return 0
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        first,second = 0,1
+        for i in range(2, number + 1):
+            next_number = first + second
+            first = second
+            second = next_number
+        return second
 
 if __name__ == "__main__":
     print(fibonacci(0))  # 0
