f22c5436bf56c051d4f571ff62cea27357e6c50d
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 00:57:49 +0700

    add answer

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,18 @@
 def primeX(x):
-    return 0
+    prime_count = 0
+    number = 2
+    while True:
+        is_prime = True
+        for i in range( 2, int(number ** 0.5)+1 ):
+            if number % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            prime_count += 1
+            if prime_count == x:
+                return number 
+        number += 1
 
 if __name__ == "__main__":
     print(primeX(1))  # 2
