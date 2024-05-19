0be712711829280a9dee059740395f76766444c8
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 14:08:03 +0700

    add solution

--- problem4/main.py
+++ problem4/main.py
@@ -1,6 +1,23 @@
 def generate_primes_grid(width, height, start):
-    result = ""
-    return result
+    result = [[0] * width for _ in range(height)]
+    num = start + 1
+    prime_found = 0
+    
+    while prime_found < width * height:
+        if num > 1:
+            is_prime = True
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                row = prime_found // width
+                coloum = prime_found % width
+                if row < height and coloum < width:
+                    result[row][coloum] = num
+                prime_found += 1
+        num += 1
+    return "\n".join(" ".join(map(str, row)) for row in result) + "\n"
 
 if __name__ == "__main__":
     print(generate_primes_grid(2, 3, 13))
